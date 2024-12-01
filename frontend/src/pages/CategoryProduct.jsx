969296d7bb125618909f7701c900069afdcf1043
AJITH B <139620991+Ajith-Balan@users.noreply.github.com>
2024-12-01 20:04:22 +0530

    Update CategoryProduct.jsx

--- frontend/src/pages/CategoryProduct.jsx
+++ frontend/src/pages/CategoryProduct.jsx
@@ -25,7 +25,7 @@ const CategoryProduct = () => {
     return (
         <Layout>
             
-           <div className="container mx-auto px-4 py-8">
+           <div className="container mx-auto px-4 mt-5 py-8">
     <h1 className="text-3xl font-bold mb-8 text-center">{category.name}</h1>
     <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
             {products?.map((p) => (
