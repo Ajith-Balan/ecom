c7369712ee4a9619431ce6148971ed5650898765
AJITH B <139620991+Ajith-Balan@users.noreply.github.com>
2024-12-14 21:33:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connection from './connection.js';
-import auth from './router/auth.js';
-import categoryRoutes from './router/categoryRoutes.js';
-import productRoutes from './router/productRoutes.js';
-import cors from 'cors';
-import cron from 'node-cron';
-import { MongoClient } from 'mongodb';
+import express from 'express'
+import dotenv from 'dotenv'
+import connection from './connection.js'
+import auth from './router/auth.js'
+import categoryRoutes from './router/categoryRoutes.js'
+import productRoutes from './router/productRoutes.js'
+import cors from 'cors'
 
-dotenv.config();
-connection();
 
-const app = express();
-app.use(cors());
+dotenv.config()
+connection()
+const app =express()
+app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/category', categoryRoutes);
-app.use('/api/v1/product', productRoutes);
 
-// MongoDB connection for pinging
-const client = new MongoClient(process.env.dburl, {
-  useNewUrlParser: true,
-  
-});
+app.use('/api/v1/auth',auth)
+app.use('/api/v1/category',categoryRoutes);
+// app.get('/',(req,res)=>{
+//     res.send({message:'welcome to ecom app'})
+// })
+app.use('/api/v1/product', productRoutes)
 
-// Function to connect and ping MongoDB
-async function pingDatabase() {
-  try {
-    if (!client.isConnected()) {
-      console.log('Connecting to MongoDB...');
-      await client.connect();
-    }
-    const db = client.db(); // Use default database if not specified
-    await db.command({ ping: 1 });
-    console.log('Ping successful: MongoDB connection is alive.');
-  } catch (error) {
-    console.error('Ping failed:', error);
-  }
-}
 
-// Cron job to ping MongoDB every 5 minutes
-cron.schedule('*/5 * * * *', pingDatabase);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 1000
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`server running on ${PORT}`);
+    
+})
